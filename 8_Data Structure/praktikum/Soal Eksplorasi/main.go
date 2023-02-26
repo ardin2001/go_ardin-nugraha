ff95078c10268ebd12c8941ef6d411366eef895b
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-02-26 11:25:07 +0700

    final tugas section 8

--- 8_Data Structure/praktikum/Soal Eksplorasi/main.go
+++ 8_Data Structure/praktikum/Soal Eksplorasi/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+func ArrayMulti(arr [][]int) []int {
+	diagonal_kiri := 0
+	diagonal_kanan := 0
+	for i, data1 := range arr {
+		for j := range arr {
+			if i == j {
+				diagonal_kiri += data1[i]
+			}
+			if i+j == len(arr)-1 {
+				diagonal_kanan += arr[i][j]
+			}
+		}
+	}
+	return []int{diagonal_kiri, diagonal_kanan}
+}
 func main() {
-	fmt.Println("masih proses")
+	diagonal := ArrayMulti([][]int{{1, 2, 3}, {4, 5, 6}, {9, 8, 9}})
+	fmt.Println("Diagonal Kiri :", diagonal[0])
+	fmt.Println("Diagonal Kanan :", diagonal[1])
+	result := math.Abs(float64(diagonal[0] - diagonal[1]))
+	fmt.Println("Perbedaan mutlak adalah", diagonal[0], "-", diagonal[1], "=", result)
+
 }
